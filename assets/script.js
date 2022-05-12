fc84c231bd95fe0050ed94a51dbe8896175960ae
Tonyavall <tonyavallescas@gmail.com>
2022-05-11 18:35:51 -0700

    Finished fetchWeatherPast function

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,7 @@ let statusContainer = document.getElementById('status-container')
 let uvContainer = document.querySelector('.uv-container')
 let statusDate = document.querySelector('.status-date')
 let uvP = document.querySelector('.uv-p')
+let forecastDayContainers = document.querySelectorAll('#forecast-day')
 
 // Pulling current date
 
@@ -38,7 +39,7 @@ let appendContent = (obj) => {
     let content = document.createElement(obj.tag)
     let keys = Object.keys(obj.setAttr)
     let values = Object.values(obj.setAttr)
-    
+
     //Looping over object setAttr keys and values and setting it as contents attributes
     for (var i = 0; i < keys.length; i++) {
         content.setAttribute(keys[i], values[i])
@@ -73,7 +74,7 @@ function apiCondition() {
     }
 }
 
-function fetchCords() {    
+function fetchCords() {
     let apiUrl = apiCondition()
     return fetch(apiUrl)
         .then(response => {
@@ -102,10 +103,10 @@ async function fetchWeatherCurrent() {
             return response.json()
         })
         .then(response => {
-        // Appending reponse data to status container
+            // Appending reponse data to status container
             statusDate.textContent = cordsResults.city + ' ' + '(' + grabDate() + ')';
 
-            const temp = {
+            const TEMP = {
                 tag: 'p',
                 setAttr: {
                     class: 'temp',
@@ -113,9 +114,9 @@ async function fetchWeatherCurrent() {
                 textContent: 'Temperature: ' + kelvinToFar(response.current.temp) + 'F',
                 appendTo: statusContainer,
             }
-            appendContent(temp)
+            appendContent(TEMP)
 
-            const wind = {
+            const WIND = {
                 tag: 'p',
                 setAttr: {
                     class: 'wind',
@@ -123,9 +124,9 @@ async function fetchWeatherCurrent() {
                 textContent: 'Wind Speed: ' + response.current.wind_speed + ' MPH',
                 appendTo: statusContainer,
             }
-            appendContent(wind)
+            appendContent(WIND)
 
-            const humidity = {
+            const HUMIDITY = {
                 tag: 'p',
                 setAttr: {
                     class: 'humidity',
@@ -133,10 +134,10 @@ async function fetchWeatherCurrent() {
                 textContent: 'Humidity: ' + response.current.humidity + '%',
                 appendTo: statusContainer,
             }
-            appendContent(humidity)
+            appendContent(HUMIDITY)
 
             uvP.textContent = 'UV Index:'
-            const uvIndex = {
+            const UVINDEX = {
                 tag: 'p',
                 setAttr: {
                     class: 'uv-index',
@@ -144,7 +145,7 @@ async function fetchWeatherCurrent() {
                 textContent: response.current.uvi,
                 appendTo: uvContainer,
             }
-            appendContent(uvIndex)
+            appendContent(UVINDEX)
             // uvIndexColor()
         })
         .catch(err => {
@@ -152,12 +153,325 @@ async function fetchWeatherCurrent() {
         })
 }
 
-// async function fetchWeatherPast() {
-//     const 
+async function fetchWeatherPast() {
+    await fetchCords()
 
-//     apiUrl = 'https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={lat}&lon={lon}&dt={time}&appid={API key}'
+    const YESTERDAY = grabUnix() - 86400;
+    apiUrl = 'https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=' + cords.lat + '&lon=' + cords.lon + '&dt=' + YESTERDAY + '&appid=' + apiKey
 
-// }
+    fetch(apiUrl)
+        .then(response => {
+            return response.json()
+        })
+        .then(response => {
+            // Appending reponse data to past forecast day containers
+            const DATE = {
+                tag: 'h3',
+                setAttr: {
+                    class: 'fore-date',
+                },
+                textContent: unixToDate(response.current.dt),
+                appendTo: forecastDayContainers[0],
+            }
+            appendContent(DATE)
+
+            const ICON_CONTAINER = {
+                tag: 'div',
+                setAttr: {
+                    id: 'weather-icon',
+                    style: "background-image: url('http://openweathermap.org/img/w/" + response.current.weather[0].icon + ".png'); background-repeat: no-repeat;",
+                },
+                appendTo: forecastDayContainers[0],
+            }
+            appendContent(ICON_CONTAINER)
+
+            const TEMP = {
+                tag: 'p',
+                setAttr: {
+                    class: 'fore-temp',
+                },
+                textContent: 'Temp: ' + kelvinToFar(response.current.temp) + 'F',
+                appendTo: forecastDayContainers[0]
+            }
+            appendContent(TEMP)
+
+            const WIND = {
+                tag: 'p',
+                setAttr: {
+                    class: 'fore-wind',
+                },
+                textContent: 'Wind: ' + response.current.wind_speed + 'MPH',
+                appendTo: forecastDayContainers[0]
+            }
+            appendContent(WIND)
+
+            const HUMIDITY = {
+                tag: 'p',
+                setAttr: {
+                    class: 'fore-humidity',
+                },
+                textContent: 'Humidity: ' + response.current.humidity + '%',
+                appendTo: forecastDayContainers[0]
+            }
+            appendContent(HUMIDITY)
+        })
+        .catch(err => {
+            console.error(err)
+        })
+
+    const DAYTWO = YESTERDAY - 86400;
+    apiUrl = 'https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=' + cords.lat + '&lon=' + cords.lon + '&dt=' + DAYTWO + '&appid=' + apiKey
+
+    fetch(apiUrl)
+        .then(response => {
+            return response.json()
+        })
+        .then(response => {
+            // Appending reponse data to past forecast day containers
+            const DATE = {
+                tag: 'h3',
+                setAttr: {
+                    class: 'fore-date',
+                },
+                textContent: unixToDate(response.current.dt),
+                appendTo: forecastDayContainers[1],
+            }
+            appendContent(DATE)
+
+            const ICON_CONTAINER = {
+                tag: 'div',
+                setAttr: {
+                    id: 'weather-icon',
+                    style: "background-image: url('http://openweathermap.org/img/w/" + response.current.weather[0].icon + ".png'); background-repeat: no-repeat;",
+                },
+                appendTo: forecastDayContainers[1],
+            }
+            appendContent(ICON_CONTAINER)
+
+            const TEMP = {
+                tag: 'p',
+                setAttr: {
+                    class: 'fore-temp',
+                },
+                textContent: 'Temp: ' + kelvinToFar(response.current.temp) + 'F',
+                appendTo: forecastDayContainers[1]
+            }
+            appendContent(TEMP)
+
+            const WIND = {
+                tag: 'p',
+                setAttr: {
+                    class: 'fore-wind',
+                },
+                textContent: 'Wind: ' + response.current.wind_speed + 'MPH',
+                appendTo: forecastDayContainers[1]
+            }
+            appendContent(WIND)
+
+            const HUMIDITY = {
+                tag: 'p',
+                setAttr: {
+                    class: 'fore-humidity',
+                },
+                textContent: 'Humidity: ' + response.current.humidity + '%',
+                appendTo: forecastDayContainers[1]
+            }
+            appendContent(HUMIDITY)
+        })
+        .catch(err => {
+            console.error(err)
+        })
+
+    const DAYTHREE = DAYTWO - 86400;
+    apiUrl = 'https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=' + cords.lat + '&lon=' + cords.lon + '&dt=' + DAYTHREE + '&appid=' + apiKey
+
+    fetch(apiUrl)
+        .then(response => {
+            return response.json()
+        })
+        .then(response => {
+            // Appending reponse data to past forecast day containers
+            const DATE = {
+                tag: 'h3',
+                setAttr: {
+                    class: 'fore-date',
+                },
+                textContent: unixToDate(response.current.dt),
+                appendTo: forecastDayContainers[2],
+            }
+            appendContent(DATE)
+
+            const ICON_CONTAINER = {
+                tag: 'div',
+                setAttr: {
+                    id: 'weather-icon',
+                    style: "background-image: url('http://openweathermap.org/img/w/" + response.current.weather[0].icon + ".png'); background-repeat: no-repeat;",
+                },
+                appendTo: forecastDayContainers[2],
+            }
+            appendContent(ICON_CONTAINER)
+
+            const TEMP = {
+                tag: 'p',
+                setAttr: {
+                    class: 'fore-temp',
+                },
+                textContent: 'Temp: ' + kelvinToFar(response.current.temp) + 'F',
+                appendTo: forecastDayContainers[2]
+            }
+            appendContent(TEMP)
+
+            const WIND = {
+                tag: 'p',
+                setAttr: {
+                    class: 'fore-wind',
+                },
+                textContent: 'Wind: ' + response.current.wind_speed + 'MPH',
+                appendTo: forecastDayContainers[2]
+            }
+            appendContent(WIND)
+
+            const HUMIDITY = {
+                tag: 'p',
+                setAttr: {
+                    class: 'fore-humidity',
+                },
+                textContent: 'Humidity: ' + response.current.humidity + '%',
+                appendTo: forecastDayContainers[2]
+            }
+            appendContent(HUMIDITY)
+        })
+        .catch(err => {
+            console.error(err)
+        })
+
+    const DAYFOUR = DAYTHREE - 86400;
+    apiUrl = 'https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=' + cords.lat + '&lon=' + cords.lon + '&dt=' + DAYFOUR + '&appid=' + apiKey
+
+    fetch(apiUrl)
+        .then(response => {
+            return response.json()
+        })
+        .then(response => {
+            // Appending reponse data to past forecast day containers
+            const DATE = {
+                tag: 'h3',
+                setAttr: {
+                    class: 'fore-date',
+                },
+                textContent: unixToDate(response.current.dt),
+                appendTo: forecastDayContainers[3],
+            }
+            appendContent(DATE)
+
+            const ICON_CONTAINER = {
+                tag: 'div',
+                setAttr: {
+                    id: 'weather-icon',
+                    style: "background-image: url('http://openweathermap.org/img/w/" + response.current.weather[0].icon + ".png'); background-repeat: no-repeat;",
+                },
+                appendTo: forecastDayContainers[3],
+            }
+            appendContent(ICON_CONTAINER)
+
+            const TEMP = {
+                tag: 'p',
+                setAttr: {
+                    class: 'fore-temp',
+                },
+                textContent: 'Temp: ' + kelvinToFar(response.current.temp) + 'F',
+                appendTo: forecastDayContainers[3]
+            }
+            appendContent(TEMP)
+
+            const WIND = {
+                tag: 'p',
+                setAttr: {
+                    class: 'fore-wind',
+                },
+                textContent: 'Wind: ' + response.current.wind_speed + 'MPH',
+                appendTo: forecastDayContainers[3]
+            }
+            appendContent(WIND)
+
+            const HUMIDITY = {
+                tag: 'p',
+                setAttr: {
+                    class: 'fore-humidity',
+                },
+                textContent: 'Humidity: ' + response.current.humidity + '%',
+                appendTo: forecastDayContainers[3]
+            }
+            appendContent(HUMIDITY)
+        })
+        .catch(err => {
+            console.error(err)
+        })
+
+    const DAYFIVE = DAYFOUR - 86400;
+    apiUrl = 'https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=' + cords.lat + '&lon=' + cords.lon + '&dt=' + DAYFIVE + '&appid=' + apiKey
+
+    fetch(apiUrl)
+        .then(response => {
+            return response.json()
+        })
+        .then(response => {
+            // Appending reponse data to past forecast day containers
+            const DATE = {
+                tag: 'h3',
+                setAttr: {
+                    class: 'fore-date',
+                },
+                textContent: unixToDate(response.current.dt),
+                appendTo: forecastDayContainers[4],
+            }
+            appendContent(DATE)
+
+            const ICON_CONTAINER = {
+                tag: 'div',
+                setAttr: {
+                    id: 'weather-icon',
+                    style: "background-image: url('http://openweathermap.org/img/w/" + response.current.weather[0].icon + ".png'); background-repeat: no-repeat;",
+                },
+                appendTo: forecastDayContainers[4],
+            }
+            appendContent(ICON_CONTAINER)
+
+            const TEMP = {
+                tag: 'p',
+                setAttr: {
+                    class: 'fore-temp',
+                },
+                textContent: 'Temp: ' + kelvinToFar(response.current.temp) + 'F',
+                appendTo: forecastDayContainers[4]
+            }
+            appendContent(TEMP)
+
+            const WIND = {
+                tag: 'p',
+                setAttr: {
+                    class: 'fore-wind',
+                },
+                textContent: 'Wind: ' + response.current.wind_speed + 'MPH',
+                appendTo: forecastDayContainers[4]
+            }
+            appendContent(WIND)
+
+            const HUMIDITY = {
+                tag: 'p',
+                setAttr: {
+                    class: 'fore-humidity',
+                },
+                textContent: 'Humidity: ' + response.current.humidity + '%',
+                appendTo: forecastDayContainers[4]
+            }
+            appendContent(HUMIDITY)
+        })
+        .catch(err => {
+            console.error(err)
+        })
+
+}
 
 // function uvIndexColor() {
 
@@ -165,8 +479,21 @@ async function fetchWeatherCurrent() {
 
 // Converting Kelvin to F
 function kelvinToFar(k) {
-    f = (1.8*(k-273.15)) + 32;
+    f = (1.8 * (k - 273.15)) + 32;
     return parseInt(f)
+}
+
+// Converting unix time to date
+function unixToDate(unix) {
+    let unixT = unix
+    let date = new Date(unixT * 1000)
+
+    let dd = String(date.getDate()).padStart(2, '0');
+    let mm = String(date.getMonth() + 1).padStart(2, '0');
+    let yyyy = date.getFullYear();
+
+    date = mm + '/' + dd + '/' + yyyy;
+    return date
 }
 
 // Grabbing current date https://stackoverflow.com/questions/1531093/how-do-i-get-the-current-date-in-javascript#:~:text=Use%20new%20Date()%20to,the%20current%20date%20and%20time.&text=This%20will%20give%20you%20today's,to%20whatever%20format%20you%20wish.
@@ -175,14 +502,18 @@ function grabDate() {
     let dd = String(today.getDate()).padStart(2, '0');
     let mm = String(today.getMonth() + 1).padStart(2, '0');
     let yyyy = today.getFullYear();
-    
+
     today = mm + '/' + dd + '/' + yyyy;
     return today
 }
 
+function grabUnix() {
+    var ts = Math.round((new Date()).getTime() / 1000);
+    return ts
+}
 
 // Handles form submission and stores user data into the local storage
-let formSubmitHandler = function(event) {
+let formSubmitHandler = function (event) {
     event.preventDefault();
 
     // need to JSON.stringify() data and then JSON.parse()
@@ -196,12 +527,12 @@ let formSubmitHandler = function(event) {
 
     if (state === '') {
         fetchWeatherCurrent();
-        // fetchWeatherPast();
+        fetchWeatherPast();
     } else {
         inputContainer.state = state;
         // localStorage.setItem('state', state)
         fetchWeatherCurrent();
-        // fetchWeatherPast();
+        fetchWeatherPast();
     }
 }
 
